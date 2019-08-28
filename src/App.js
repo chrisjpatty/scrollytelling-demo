940f25bfe5a600a80046e96f6b512446bbf95d0d
Christopher Patty <christopher.jon.patty@gmail.com>
2019-08-28 12:51:59 -0600

    Initial commit from Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'normalize.css';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <header>Welcome to React</header>
+      <main>Edit App.js to get started</main>
+      <footer>This template comes with Emotion and css resets included</footer>
+    </div>
+  );
+};
+
+export default App;
